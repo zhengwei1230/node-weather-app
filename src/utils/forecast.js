a094e0ff620b66a285136bbbb79867927dd6fdfe
zhengwei1230 <zhengwei_1230@hotmail.com>
2021-02-20 23:30:41 +0800

    Add weather data

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,10 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location')
         } else {
-            callback(undefined, 'Temperature is ' + body.current.temperature + ', feels like ' + body.current.feelslike)
+            console.log(body.current)
+            callback(
+                undefined, 
+                body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degrees out. It feels like ' + body.current.feelslike + " degrees out. The humidity is " + body.current.humidity + "%.")
         }
     })
 }
